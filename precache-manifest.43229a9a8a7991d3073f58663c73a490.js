f745e267e4b9487b7a255dd53ec9242803c801a8
Savez Siddiqui <savezsiddiqui@gmail.com>
2019-06-25 12:40:41 +0530

    Updates

---
+++ precache-manifest.43229a9a8a7991d3073f58663c73a490.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7f7b3f535ea54a78ec92424384205f",
+    "url": "/Ristorante/index.html"
+  },
+  {
+    "revision": "7208553d53bef6c02501",
+    "url": "/Ristorante/static/css/2.23ebbe76.chunk.css"
+  },
+  {
+    "revision": "763c44a714b12a72f646",
+    "url": "/Ristorante/static/css/main.d8a1a198.chunk.css"
+  },
+  {
+    "revision": "7208553d53bef6c02501",
+    "url": "/Ristorante/static/js/2.46991738.chunk.js"
+  },
+  {
+    "revision": "763c44a714b12a72f646",
+    "url": "/Ristorante/static/js/main.0db0f3f2.chunk.js"
+  },
+  {
+    "revision": "5e229a6dcdff612519a8",
+    "url": "/Ristorante/static/js/runtime~main.1aa5791b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Ristorante/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Ristorante/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Ristorante/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Ristorante/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Ristorante/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
